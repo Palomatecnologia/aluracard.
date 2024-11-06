5c84b1269ff22fd20867cb300c4b504f2abdace7
Palomatecnologia <00001201466805sp@al.educacao.sp.gov.br>
2024-11-06 06:15:33 -0500

    .

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'História',
+    'Quem descobriu o Brasil?',
+    'Pedro Alvares Cabral'
+)
+
+criaCartao(
+    'quimica',
+    'Atualmente, quantos elementos químicos a tabela periódica possui?',
+    '118'
+)
+
+criaCartao(
+    'geografia',
+    'Quais os países que têm a maior e a menor expectativa de vida do mundo?',
+    'Japão e Serra Leoa'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
